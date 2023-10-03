59d19c9ad88d54e9fedd0a9934038eb4b41869b7
Marcin Poręba <marcin.poreba1995@gmail.com>
2023-10-03 13:39:45 +0200

    Add ip voter verification

--- controllers/photos.controller.js
+++ controllers/photos.controller.js
@@ -1,4 +1,7 @@
-const Photo = require('../models/Photo.model');
+const Photo = require('../models/photo.model');
+const sanitizeHtml = require('sanitize-html');
+const Voter = require('../models/Voter.model');
+const requestIp = require('request-ip');
 
 /****** SUBMIT PHOTO ********/
 
@@ -8,15 +11,42 @@ exports.add = async (req, res) => {
     const { title, author, email } = req.fields;
     const file = req.files.file;
 
-    if(title && author && email && file) { // if fields are not empty...
+    if(title && author && email && file) {
+      const sanitizedTitle = sanitizeHtml(title, { allowedTags: [], allowedAttributes: {} });
+      const sanitizedAuthor = sanitizeHtml(author, { allowedTags: [], allowedAttributes: {} });
 
-      const fileName = file.path.split('/').slice(-1)[0]; // cut only filename from full path, e.g. C:/test/abc.jpg -> abc.jpg
-      const newPhoto = new Photo({ title, author, email, src: fileName, votes: 0 });
-      await newPhoto.save(); // ...save new photo in DB
-      res.json(newPhoto);
+      const emailRegex = /^[A-Za-z0-9_!#$%&'*+\/=?`{|}~^.-]+@[A-Za-z0-9.-]+$/;
 
+
+      if (
+          emailRegex.test(email) &&
+          sanitizedTitle.length <= 25 &&
+          sanitizedAuthor.length <= 50
+      ) {
+
+        const fileName = file.path.split('/').slice(-1)[0];
+        const fileExt = fileName.split('.').slice(-1)[0];
+
+        if ((fileExt === 'jpg' || fileExt === 'png' || fileExt === 'gif')) {
+
+          const newPhoto = new Photo({
+            title: sanitizedTitle,
+            author: sanitizedAuthor,
+            email,
+            src: fileName,
+            votes: 0,
+          });
+          await newPhoto.save(); // ...save new photo in DB
+          res.json(newPhoto);
+        } else {
+          throw new Error('Wrong file format!');
+        }
+
+      } else {
+        throw new Error('Wrong input!');
+      }
     } else {
-      throw new Error('Wrong input!');
+      throw new Error('Missing required fields!');
     }
 
   } catch(err) {
@@ -42,12 +72,31 @@ exports.loadAll = async (req, res) => {
 exports.vote = async (req, res) => {
 
   try {
+
     const photoToUpdate = await Photo.findOne({ _id: req.params.id });
-    if(!photoToUpdate) res.status(404).json({ message: 'Not found' });
-    else {
+    const clientIp = requestIp.getClientIp(req);
+    const voter = await Voter.findOne({ user: clientIp })
+
+    if (!voter) {
+
+      const newVoter = new Voter({ user: clientIp, votes: photoToUpdate._id })
+      await newVoter.save();
       photoToUpdate.votes++;
       photoToUpdate.save();
       res.send({ message: 'OK' });
+
+    } else {
+
+      if(voter.votes.includes(photoToUpdate._id)) {
+        throw new Error('You have already voted...!');
+
+      } else {
+        voter.votes.push(photoToUpdate._id);
+        voter.save();
+        photoToUpdate.votes++;
+        photoToUpdate.save();
+        res.send({ message: 'OK' });
+      }
     }
   } catch(err) {
     res.status(500).json(err);
